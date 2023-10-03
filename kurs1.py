c9b741873fc2cfd9bfce3df51df1dc92e9d7866e
Katarzyna Sikorska <katarzyna.sikorska@pl.abb.com>
2023-10-03 15:34:47 +0200

    dodano kurs1 czyli kartka urodzinowa

---
+++ kurs1.py
@@ -0,0 +1,48 @@
+print("Wygeneruj wyjątkową kartkę z życzeniami")
+
+imię = input("wpisz imię Jubilata, któremu chcesz złożyć życzenia:")
+
+rok_urodzenia = int(input("podaj rok urodzenia jubilata:"))
+
+wiadomość = input("wpisz wiadomość, którą chcesz przesłać do Jubilata:")
+
+imię_nadawcy = input("wpisz swoje imię:")
+
+obecny_rok = 2023
+
+wiek_jubilata = obecny_rok - rok_urodzenia
+
+print(f"{imię}, wszystkiego najlepszego z okazji {wiek_jubilata} urodzin!")
+
+print(f"{wiadomość}")
+
+print(f"{imię_nadawcy}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
